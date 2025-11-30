82b355ad0172e027ec1329fe10df569a6faee2c1
blackstaramp2021-cell <blackstaramp2021@gmail.com>
2025-11-30 21:23:37 +0900

    Auto commit

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ let clickCount = 0;
 // プロフィール画像がクリックされたら実行される関数
 function profileClicked() {
   clickCount++; // カウントを1増やす
-  alert("プロフィール画像が " + clickCount + " 回クリックされました！");
+  // HTML上の表示を書き換える
+  document.getElementById("clickDisplay").textContent =
+    "クリック回数：" + clickCount;
 }
